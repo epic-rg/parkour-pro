d416569b91137c688b589cf8734baa1e9d32118e
epic-rg <68272836+epic-rg@users.noreply.github.com>
2021-12-23 15:57:20 +0530

    Add files via upload
    movement script added

---
+++ PlayerMovement.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    public float speed = 12f;
+    public float gravity = -9.81f ;
+    public float jumpHeight = 3f;
+
+    public bool isSprinting = false;
+    public float sprintingMultiplier = 2f;
+    public void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position , groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime );
+
+
+        if(Input.GetButtonDown("Jump") && isGrounded )
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            isSprinting = true;
+        }
+        else
+        {
+            isSprinting = false;
+        }
+
+
+        if(isSprinting ==  true )
+        {
+            move *= sprintingMultiplier;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+    }
+}
